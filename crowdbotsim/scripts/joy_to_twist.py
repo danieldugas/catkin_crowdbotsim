ba0a7dc848a3ea9f82203dff49915099f0be76e3
Fabien Grzeskowiak <fabien.grzeskowiak@inria.fr>
2019-06-03 15:25:51 +0200

    wip

--- crowdbotsim/scripts/joy_to_twist.py
+++ crowdbotsim/scripts/joy_to_twist.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
-    
+
+script, topic = sys.argv    
 
 # initialize node
 rospy.init_node('joy_to_twist', anonymous = True)
 
 
 #### Setup joy_to_twist Publisher 
-joy_to_twist_pub = rospy.Publisher("pos_robot", Twist, queue_size = 5)
+joy_to_twist_pub = rospy.Publisher(topic, Twist, queue_size = 5)
 
 msg = Twist()
 
 def on_joy(joy):
-    msg.linear.x = joy.axes[1]
-    msg.angular.z = -joy.axes[0]*180/3.141592
+    msg.linear.y = joy.axes[1]
+    msg.angular.z = joy.axes[0]*180/3.141592
     joy_to_twist_pub.publish(msg)
 
 def joy_to_twist():
@@ -24,11 +26,12 @@ def joy_to_twist():
         #### Setup joy subscriber
         joy_sub = rospy.Subscriber('joy', Joy, on_joy)
 
-	rate = rospy.Rate(10) # 10hz
+        rate = rospy.Rate(10) # 10hz
 
 	while not rospy.is_shutdown():
             #### Publish msg
             rospy.loginfo(msg)
+            rate.sleep()
 
 
 if __name__ == '__main__':
